33523b653d3351fadbd85fff5b62463d368c189f
Jakub Gac <jakubgac@Mac-Pro-Jakub.local>
2017-07-20 18:35:33 +0200

    View has been created.

---
+++ Banking App/AccountsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AccountsViewController.swift
+//  Banking App
+//
+//  Created by Jakub Gac on 20.07.2017.
+//  Copyright © 2017 Jakub Gac. All rights reserved.
+//
+
+import UIKit
+
+class AccountsViewController: UIViewController {
+
+    @IBAction func cancelAccountCreation(segue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func saveAccount(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
