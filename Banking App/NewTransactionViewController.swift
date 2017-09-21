e27664412ec74b083a6b388279111c70337c4b18
Jakub Gac <jakubgac@mac-pro-jakub.local>
2017-09-21 22:42:24 +0200

    View finished. NewTransactionController also done.

--- Banking App/NewTransactionViewController.swift
+++ Banking App/NewTransactionViewController.swift
@@ -10,20 +10,48 @@ import UIKit
 
 class NewTransactionViewController: UIViewController {
 
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet var ringViews: [UIView]!
+    @IBOutlet var categoryButtons: [UIButton]!
     
+    var selectedIndex = 0
+    
+    var amount: Float? {
+        guard let text = amountTextField.text else {
+            return nil
+        }
+        return Float(text)
+    }
+    
+    var transactionDescription: String? {
+        return descriptionTextField.text
+    }
+    
+    var selectedCategory: TransactionCategory? {
+        switch selectedIndex {
+        case 0: return .income
+        case 1: return .groceries
+        case 2: return .utilities
+        case 3: return .home
+        case 4: return .transport
+        case 5: return .fun
+        default: return nil
+        }
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        resetRings()
     }
     
-
+    @IBAction func selectCategory(_ sender: UIButton) {
+        print("Przycisk wcisniety")
+        resetRings()
+        selectedIndex = categoryButtons.index(of: sender)!
+        let selectedRingView = ringViews[selectedIndex]
+        selectedRingView.layer.borderColor = UIColor.skyBlue.cgColor
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -33,5 +61,30 @@ class NewTransactionViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension NewTransactionViewController {
+    func resetRings() {
+        for ringView in ringViews {
+            ringView.layer.borderColor = UIColor.clear.cgColor
+        }
+    }
+}
+
+extension TransactionCategory {
+    struct Appearance {
+        let color: UIColor
+        let image: UIImage
+    }
+    
+    var appearance: Appearance {
+        switch self {
+        case .income: return Appearance(color: .lightGrassGreen, image: #imageLiteral(resourceName: "income"))
+        case .groceries: return Appearance(color: .seafoamBlue, image: #imageLiteral(resourceName: "groceries"))
+        case .utilities: return Appearance(color: .liliac, image: #imageLiteral(resourceName: "utilities"))
+        case .home: return Appearance(color: .sunflowerYellow, image: #imageLiteral(resourceName: "home"))
+        case .transport: return Appearance(color: .peach, image: #imageLiteral(resourceName: "transport"))
+        case .fun: return Appearance(color: .carnation, image: #imageLiteral(resourceName: "fun"))
+        }
+    }
 }
